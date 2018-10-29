f3ae4eea5876436aaa821a6b44b398964742caea
Yehuda <yehudabukhantsov@gmail.com>
2018-10-29 19:47:00 +0200

    fixes

--- src/main/java/com/travelfactory/catalog/configs/MongoConfig.java
+++ src/main/java/com/travelfactory/catalog/configs/MongoConfig.java
@@ -1,6 +1,5 @@
 package com.travelfactory.catalog.configs;
 
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
 import de.flapdoodle.embed.mongo.MongodExecutable;
@@ -12,10 +11,7 @@ import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -24,7 +20,9 @@ import java.net.UnknownHostException;
 
 @Configuration
 public class MongoConfig {
+
     private static final String MONGO_DB_URL = "localhost";
+    private static final int MONGO_DB_PORT = 12345;
     private static final String MONGO_DB_NAME = "catalog";
 
     @Bean
@@ -36,24 +34,8 @@ public class MongoConfig {
         return mongoTemplate;
     }
 
-//    private static final String MONGO_DB_URL = "localhost";
-    private static final int MONGO_DB_PORT = 12345;
-
     MongodStarter starter = MongodStarter.getDefaultInstance();
     MongodExecutable mongodExecutable;
-
-//    @Bean
-//    public MongoDbFactory mongoDbFactory() throws UnknownHostException {
-//        Mongo mongo = new MongoClient(MONGO_DB_URL, MONGO_DB_PORT);
-//        UserCredentials userCredentials = new UserCredentials("", ""); // username and password
-//        return new SimpleMongoDbFactory(mongo, "store", userCredentials);
-//    }
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate() throws UnknownHostException {
-//        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-//        return mongoTemplate;
-//    }
 
     @PostConstruct
     public void construct() throws UnknownHostException, IOException {
@@ -68,4 +50,5 @@ public class MongoConfig {
             mongodExecutable.stop();
         }
     }
+
 }
